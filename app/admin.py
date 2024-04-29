4652344fd2a46678a1b70f122ca4cd1745d07910
Филиппов Данил <filippok2004180@gmail.com>
2024-04-29 20:44:32 +0500

    Update admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from app.models import Place, UserProfile
+
+
 # Register your models here.
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    pass
