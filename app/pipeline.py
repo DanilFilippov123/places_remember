1963726942b5a65da13b601e728da4862b487149
Филиппов Данил <filippok2004180@gmail.com>
2024-04-29 20:45:09 +0500

    Fetch image from vk

---
+++ app/pipeline.py
@@ -0,0 +1,19 @@
+import io
+
+import requests
+
+from app.models import UserProfile
+
+
+def get_avatar(backend, response, user=None, *args, **kwargs):
+    url = None
+    has_profile = UserProfile.objects.filter(user=user).exists()
+    if backend.name == 'vk-oauth2' and not has_profile:
+        url = response.get()
+        if url:
+            image = requests.get(url)
+            profile = UserProfile(user=user)
+            if image.status_code == 200:
+                profile.photo.save(name='avatar.jpg', content=io.BytesIO(image.content))
+            else:
+                profile.save()
