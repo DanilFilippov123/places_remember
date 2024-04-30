2455200b6675983f2b9fe23985a5fe5a7c684cd7
Филиппов Данил <filippok2004180@gmail.com>
2024-04-30 21:03:45 +0500

    Add navbar

--- app/pipeline.py
+++ app/pipeline.py
@@ -9,7 +9,7 @@ def get_avatar(backend, response, user=None, *args, **kwargs):
     url = None
     has_profile = UserProfile.objects.filter(user=user).exists()
     if backend.name == 'vk-oauth2' and not has_profile:
-        url = response.get()
+        url = response.get('user_photo')
         if url:
             image = requests.get(url)
             profile = UserProfile(user=user)
