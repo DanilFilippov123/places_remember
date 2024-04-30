776a9f57406bfb4a0a82cc4c430225279da0ae6a
Филиппов Данил <filippok2004180@gmail.com>
2024-05-01 00:49:56 +0500

    Add yandex maps api key to views

--- app/views.py
+++ app/views.py
@@ -1,9 +1,10 @@
+from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
 from django.core.exceptions import PermissionDenied
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, UpdateView, ListView, TemplateView
+from django.views.generic import CreateView, ListView, TemplateView, UpdateView, DeleteView
 
 from app.forms import PlaceForm, RegisterUserForm
 from app.models import Place, UserProfile
@@ -11,7 +12,7 @@ from app.models import Place, UserProfile
 
 class StartPageTemplateView(TemplateView):
     template_name = 'app/index.html'
-    
+
     def get(self, request, *args, **kwargs):
         # if user is already authenticated - redirect him to his places
         if request.user.is_authenticated:
@@ -23,16 +24,21 @@ class PlaceCreateView(LoginRequiredMixin, CreateView):
     form_class = PlaceForm
     template_name = 'app/place.html'
 
+    extra_context = {"yandex_maps_api_key": settings.YANDEX_MAPS_API_KEY}
+
+    success_url = reverse_lazy('my_places_page')
+
     def form_valid(self, form):
         place = form.save(commit=False)
         place.user = self.request.user
-        place.save()
         return super().form_valid(form)
 
 
 class PlaceUpdateView(LoginRequiredMixin, UpdateView):
     model = Place
     form_class = PlaceForm
+
+    extra_context = {"yandex_maps_api_key": settings.YANDEX_MAPS_API_KEY}
 
     template_name = 'app/place.html'
     success_url = reverse_lazy('hello_page')
@@ -42,7 +48,10 @@ class AllPlacesView(LoginRequiredMixin, ListView):
     model = Place
     template_name = 'app/user_places.html'
     context_object_name = 'places'
-    paginate_by = 10
+
+    extra_context = {"yandex_maps_api_key": settings.YANDEX_MAPS_API_KEY}
+
+    paginate_by = 3
 
     def get_queryset(self):
         user = self.request.user
@@ -69,11 +78,11 @@ class RegistrationView(CreateView):
         return super().post(request, *args, **kwargs)
 
     def form_valid(self, form):
-        redirect = super().form_valid(form)
+        _redirect = super().form_valid(form)
         user = self.object
         profile = UserProfile(user=user)
         photo = form.cleaned_data['photo']
         if photo is not None:
             profile.photo.save(name=photo.name, content=photo.file)
         profile.save()
-        return redirect
+        return _redirect
