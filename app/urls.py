d09ea6cb9417cb3fa35291499d2275f426d75001
Филиппов Данил <filippok2004180@gmail.com>
2024-05-01 00:52:01 +0500

    Update app urls

--- app/urls.py
+++ app/urls.py
@@ -6,8 +6,9 @@ from app import views
 urlpatterns = [
     path("place/", views.PlaceCreateView.as_view(), name='place_page'),
     path("place/<int:pk>/update", views.PlaceUpdateView.as_view(), name='place_update_page'),
+    path("place/<int:pk>/delete", views.PlaceDeleteView.as_view(), name='place_delete_page'),
     path("place/my", views.AllPlacesView.as_view(), name='my_places_page'),
-    path("login", views.PlaceLoginView.as_view(), name='login_page'),
+    path("login", views.PlaceRememberLoginView.as_view(), name='login_page'),
     path("logout", LogoutView.as_view(), name='logout_page'),
     path("registration", views.RegistrationView.as_view(), name='registration_page'),
     path('', views.StartPageTemplateView.as_view(), name='hello_page'),
